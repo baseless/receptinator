f20d3b804dd365089c58b5d5016f97b3ae600097
Mattias <maze.89@hotmail.com>
2016-01-08 18:35:34 +0100

    Fixed some issues...

--- src/main/java/nu/njp/receptinator/entities/BaseEntity.java
+++ src/main/java/nu/njp/receptinator/entities/BaseEntity.java
@@ -1,11 +1,15 @@
 package nu.njp.receptinator.entities;
 
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by Mattias on 2016-01-08.
  */
 
-public class BaseEntity {
+public abstract class BaseEntity {
 
     public enum Status { ACTIVE, INACTIVE };
 
